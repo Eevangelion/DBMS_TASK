98ea72c80dac9456378299643db61f72ec0b4af9
TheFairesko <81828604+TheFairesko@users.noreply.github.com>
2022-12-26 12:04:06 +0300

    Fixed API

--- internal/models/userResponse.go
+++ internal/models/userResponse.go
@@ -1,9 +1,10 @@
 package models
 
 type UserResponse struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Reports     int    `json:"reports"`
-	Favorites   int    `json:"remainingReports"`
-	LastBanDate string `json:"lastBanDate"`
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Role      string `json:"role"`
+	Reports   int    `json:"reports"`
+	Favorites int    `json:"remaining_reports"`
+	UnbanDate string `json:"unban_date"`
 }
