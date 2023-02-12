dbe0454043e3c861a513d3457193383656664181
TheFairesko <81828604+TheFairesko@users.noreply.github.com>
2023-02-12 19:55:25 +0300

    Fixed joke creation API

--- internal/models/jokeRequest.go
+++ internal/models/jokeRequest.go
@@ -1,6 +1,7 @@
 package models
 
 type JokeRequest struct {
+	Tags        []Tag  `json:"tags"`
 	Header      string `json:"header"`
 	Description string `json:"description"`
 	AuthorId    int    `json:"author_id"`
